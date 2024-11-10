6870e941fd9323ca5285ebd01838b5475f66bd14
FreudAnnony <60725606+FreudAnnony@users.noreply.github.com>
2024-11-10 16:06:53 +0700

    hello

--- cypress/e2e/table.js
+++ cypress/e2e/table.js
@@ -1,20 +1,20 @@
 describe('example testing table', () => {
     beforeEach(() => {
         cy.navigateTo()
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
     })
 
     it('Web tables', () => {
-        cy.contains('Tables & Data').click()
-        cy.contains('Smart Table').click()
 
         //1 Get the row by text
         cy.get('tbody').contains('tr', 'Larry').then( tableRow => {
-            
+
             cy.wrap(tableRow).find('.nb-edit').click()
             cy.wrap(tableRow).find('[placeHolder="Age"]').clear().type(35)
             cy.wrap(tableRow).find('.nb-checkmark').click()
             cy.wrap(tableRow).find('td').eq(6).should('contain', '35')
-          
+
             /**
              * Note: how to make this code work exactly like above
              * let row = cy.wrap(tableRow)
@@ -38,5 +38,24 @@ describe('example testing table', () => {
             cy.wrap(tableColumns).eq(2).should('contain', 'John')
             cy.wrap(tableColumns).eq(3).should('contain', 'Doe')
         })
+    })
+
+    it.only('validation and interator through table row', () => {
+        //Get each row validation
+        const age = [20, 30, 40, 200]
+        cy.wrap(age).each( age => {
+            cy.get('[placeholder="Age"]').clear().type(age)
+            cy.wait(500)
+            cy.get('tbody tr').each(row => {
+
+                if(age==200){
+                  cy.wrap(row).should('contain', 'No data found')
+                }else {
+                  cy.wrap(row).find('td').should('contain', age)
+                }
+
+            })
+        })
+
     })
 })
